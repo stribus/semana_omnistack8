3f58ef450be51f59fd7aad00ed1daa5a7882c0b0
Flavio <paladinotoldo@gmail.com>
2019-08-22 15:59:48 -0300

    backend

--- aula/backend/src/server.js
+++ aula/backend/src/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-mongoose.connect('mongodb://localhost:27017/tindev?retryWrites=true&w=majority',{useNewUrlParser:true});
+const connectedUsers = {};
 
-const server = express();
-server.use(express.json());
-server.use(routes);
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
 
+  connectedUsers[user] = socket.id;
+});
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-jxhrd.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
